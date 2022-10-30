16b6821d25055bae7f021228bca9b780ab0ffdc1
Jon1k <zenaserdykov2004@gmail.com>
2022-10-30 11:35:25 +0200

    anonium classtask6

--- src/task6/Main.java
+++ src/task6/Main.java
@@ -1,6 +1,7 @@
 package task6;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +12,17 @@ public class Main {
                 new Triangle(10, 15, 7),
                 new Triangle(-3, 7.13, 5)
         };
-        Arrays.sort(triangles, new CompareByArea());
+//        Arrays.sort(triangles, new CompareByArea());
+
+
+
+        // without additional clases (using anon class)
+        Arrays.sort(triangles, new Comparator<Triangle>() {
+            @Override
+            public int compare(Triangle o1, Triangle o2) {
+                return Double.compare(o1.getArea(), o2.getArea());
+            }
+        });
         for (var i : triangles) {
             System.out.println(i.toString());
         }
